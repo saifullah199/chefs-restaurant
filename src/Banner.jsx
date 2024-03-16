550f51ea0615b798a935c1214de65ecd999ce480
saifullah199 <mdkhalidsaifullah199@gmail.com>
2024-03-16 13:22:25 +0600

    navbar added

---
+++ src/Banner.jsx
@@ -0,0 +1,27 @@
+
+
+const Banner = () => {
+    return (
+        <div>
+            <div className="hero h-[400px]" style={{backgroundImage: 'url(./assets/images/cooking.jpg)', backgroundSize: 'cover', backgroundPosition: 'center'}}>
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-center text-neutral-content">
+                <div className="">
+                    <h1 className="mb-5 text-5xl font-bold">Discover an exceptional cooking <br />
+                        class tailored for you!
+                        </h1>
+                    <p className="mb-5">Learn and Master Basic Programming, Data Structures, Algorithm, OOP, Database and solve 500+ coding <br />
+                        problems to become an exceptionally well world-class Programmer.
+                        </p>
+                    <div className="">
+                        <button className="btn btn-accent">Explore Now</button>
+                        <button className="btn btn-outline ml-5 text-white">Our Feedback</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    );
+};
+
+export default Banner;
